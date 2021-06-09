771263d84d04e5755337fa465757ef19e3d8ffff
Allen George Ajith <allen200118@gmail.com>
2021-06-09 21:59:38 +0400

    Delete .gitignore directory

--- .gitignore/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Attempt to connect to MySQL database */
-$mysqli = mysqli_connect('localhost','allen','password', 'EST') or die("Some error occured while connecting to DB!");
- 
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-?>
